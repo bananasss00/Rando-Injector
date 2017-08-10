407cc90913ece5297615f5a483d328b1361b923e
bananasss00 <bananasss00@yandex.ru>
2017-08-10 21:01:45 +0300

    Sources

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Rando-Injector
+
+## How compile?
+1) Change DLL_NAME in string [6] in to your dll name
+2) Change all bytes(0x00, 0x01, 0x03....) in define "JUNKS" to random bytes
+3) Change size this block by add new bytes
+4) Compile this 
+
+## Video
+[![How copile?](https://img.youtube.com/vi/bA3CcQ4p1QQ/0.jpg)](https://www.youtube.com/watch?v=bA3CcQ4p1QQ)
---
+++ Source/RandoInjector/Main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+// Change "MY_DLL_NAME.dll" to your dll name
+#define DLL_NAME "MY_DLL_NAME.dll"
+
+// And you need:
+// 1. change all bytes(0x00, 0x01, 0x03....) in define "JUNKS" to random bytes
+// 2. change size this block by add new bytes
+#define JUNKS \
+__asm _emit 0x00 \
+__asm _emit 0x01 \
+__asm _emit 0x03 \
+__asm _emit 0x04 \
+__asm _emit 0x05 \
+__asm _emit 0x06 \
+__asm _emit 0x07 \
+__asm _emit 0x08 \
+__asm _emit 0x09 \
+
+
+// Don't change this!
+#define _JUNK_BLOCK(s) __asm jmp s JUNKS __asm s:
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//                                 INJECTOR CODE                                   //
+/////////////////////////////////////////////////////////////////////////////////////
+
+DWORD Process(char* ProcessName)
+{
+	_JUNK_BLOCK(jmp_label1)
+	HANDLE hPID = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	
+	_JUNK_BLOCK(jmp_label2)
+	PROCESSENTRY32 ProcEntry;
+	
+	_JUNK_BLOCK(jmp_label3)
+	ProcEntry.dwSize = sizeof(ProcEntry);
+	
+	_JUNK_BLOCK(jmp_label4)
+	do
+	{
+		_JUNK_BLOCK(jmp_label5)
+		if (!strcmp(ProcEntry.szExeFile, ProcessName))
+		{
+			_JUNK_BLOCK(jmp_label6)
+			DWORD dwPID = ProcEntry.th32ProcessID;
+			
+			_JUNK_BLOCK(jmp_label7)
+			CloseHandle(hPID);
+			
+			_JUNK_BLOCK(jmp_label8)
+			return dwPID;
+		}
+		
+		_JUNK_BLOCK(jmp_label9)
+	}
+	while (Process32Next(hPID, &ProcEntry));
+	
+	_JUNK_BLOCK(jmp_label10)
+}
+
+int main()
+{
+	_JUNK_BLOCK(jmp_label11)
+	DWORD dwProcess;
+	
+	_JUNK_BLOCK(jmp_label12)
+	char myDLL[MAX_PATH];
+	
+	_JUNK_BLOCK(jmp_label13)
+	GetFullPathName(DLL_NAME, MAX_PATH, myDLL, 0);
+	
+	_JUNK_BLOCK(jmp_label4)
+	dwProcess = Process("csgo.exe");
+	
+	_JUNK_BLOCK(jmp_label15)
+	HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwProcess);
+	
+	_JUNK_BLOCK(jmp_label16)
+	LPVOID allocatedMem = VirtualAllocEx(hProcess, NULL, sizeof(myDLL), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	
+	_JUNK_BLOCK(jmp_label17)
+	WriteProcessMemory(hProcess, allocatedMem, myDLL, sizeof(myDLL), NULL);
+	
+	_JUNK_BLOCK(jmp_label18)
+	CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, allocatedMem, 0, 0);
+	
+	_JUNK_BLOCK(jmp_label19)
+	CloseHandle(hProcess);
+	
+	_JUNK_BLOCK(jmp_label20)
+	return 0;
+	
+	_JUNK_BLOCK(jmp_label21)
+}
