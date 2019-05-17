cf091ddf60ab8e3158410a2286f8ab5ffa1ddeab
rrroyal <aa@shameful.xyz>
2019-05-17 23:59:00 +0200

    Updated some stuff

--- README.md
+++ README.md
@@ -1,21 +1,16 @@
 # Rando-Injector
 
 ## Changes
-**It has been edited to include interactive mode (you provide process name and dll name on the go, not hardcoding it) - that's the only change.**  
-If you want to hardcode something, follow these steps:
-### Process name
-1) Comment or delete lines **71** and **72**
-2) Edit line **68** to this: `char process[MAX_PATH] = "process_name.exe";`  
-*(replace `process_name.exe` with your process name)*
-### DLL name
-1) Comment or delete lines **74** and **75**
-2) Edit line **69** to this: `char dll[MAX_PATH] = "dll_name.dll";`  
-*(replace `dll_name.dll` with your dll name)*
+**It has been edited to include interactive mode (you provide process name and DLL name on the go, not hardcoding it).**  
 
 ## How to compile?
+### Clean version
+Just open it in VS2017/2019 and build it (Windows 10 SDK 18362, Platform Toolkit v142)
+
+### Bloated version
 1) Change all bytes (0x00, 0x01, 0x03....) in define "JUNKS" to random bytes
 2) Change size of this block by adding new bytes
-3) Compile (VS2017 - `F9` or `Ctrl+Shift+B`)
+3) Compile (VS2017/2019 - `F9` or `Ctrl+Shift+B`)
 
 ## Source
 [bananasss00/Rando-Injector](https://github.com/bananasss00/Rando-Injector)
--- Source/RandoInjector/Main.cpp
+++ Source/RandoInjector/Main.cpp
@@ -2,107 +2,56 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
-// Interactive fork by rrroyal // github.com/rrroyal
-DWORD dwProcess;
-char realDLL[MAX_PATH];
-
-// And you need:
-// 1. change all bytes(0x00, 0x01, 0x03....) in define "JUNKS" to random bytes
-// 2. change size this block by add new bytes
-#define JUNKS \
-__asm _emit 0x00 \
-__asm _emit 0x01 \
-__asm _emit 0x03 \
-__asm _emit 0x04 \
-__asm _emit 0x05 \
-__asm _emit 0x06 \
-__asm _emit 0x07 \
-__asm _emit 0x08 \
-__asm _emit 0x09 \
-
-
-// Don't change this!
-#define _JUNK_BLOCK(s) __asm jmp s JUNKS __asm s:
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-//                                 INJECTOR CODE                                   //
-/////////////////////////////////////////////////////////////////////////////////////
-
-DWORD Process(char* ProcessName)
-{
-	_JUNK_BLOCK(jmp_label1)
+DWORD getProcess(char* ProcessName) {
 	HANDLE hPID = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-	
-	_JUNK_BLOCK(jmp_label2)
 	PROCESSENTRY32 ProcEntry;
-	
-	_JUNK_BLOCK(jmp_label3)
 	ProcEntry.dwSize = sizeof(ProcEntry);
 	
-	_JUNK_BLOCK(jmp_label4)
-	do
-	{
-		_JUNK_BLOCK(jmp_label5)
-		if (!strcmp(ProcEntry.szExeFile, ProcessName))
-		{
-			_JUNK_BLOCK(jmp_label6)
+	do {
+		if (!strcmp(ProcEntry.szExeFile, ProcessName)) {
 			DWORD dwPID = ProcEntry.th32ProcessID;
-			
-			_JUNK_BLOCK(jmp_label7)
 			CloseHandle(hPID);
 			
-			_JUNK_BLOCK(jmp_label8)
 			return dwPID;
 		}
-		
-		_JUNK_BLOCK(jmp_label9)
 	}
 	while (Process32Next(hPID, &ProcEntry));
-	
-	_JUNK_BLOCK(jmp_label10)
 }
 
-int main()
-{
+int main() {
+	// declare variables
 	char process[MAX_PATH];
 	char dll[MAX_PATH];
+
+	DWORD dwProcess;
+	char myDLL[MAX_PATH];
 	
+	// ask for input
 	printf("process: ");
 	scanf_s("%259s", process, MAX_PATH - 1);
 
 	printf("dll: ");
 	scanf_s("%259s", dll, MAX_PATH - 1);
-	
-	_JUNK_BLOCK(jmp_label11)
-	DWORD dwProcess;
-	
-	_JUNK_BLOCK(jmp_label12)
-	char myDLL[MAX_PATH];
-	
-	_JUNK_BLOCK(jmp_label13)
+
+	// get path of DLL
 	GetFullPathNameA(dll, MAX_PATH, myDLL, NULL);
 	
-	_JUNK_BLOCK(jmp_label4)
-	dwProcess = Process(process);
+	// get process
+	dwProcess = getProcess(process);
 
-	_JUNK_BLOCK(jmp_label15)
+	// open handle and allocate memory
 	HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwProcess);
-	
-	_JUNK_BLOCK(jmp_label16)
 	LPVOID allocatedMem = VirtualAllocEx(hProcess, NULL, sizeof(myDLL), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-	
-	_JUNK_BLOCK(jmp_label17)
+
+	// actually "inject" DLL into process memory
 	WriteProcessMemory(hProcess, allocatedMem, myDLL, sizeof(myDLL), NULL);
-	
-	_JUNK_BLOCK(jmp_label18)
+
+	// launch DLL
 	CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, allocatedMem, 0, 0);
 	
-	_JUNK_BLOCK(jmp_label19)
+	// close handle
 	CloseHandle(hProcess);
 	
-	_JUNK_BLOCK(jmp_label20)
-	return 0;
-	
-	_JUNK_BLOCK(jmp_label21)
+	// done
+	return 1;
 }
